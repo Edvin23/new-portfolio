cd653779d04760628e68b1f74b59c29a854d49a7
Edvin23 <150404642+Edvin23@users.noreply.github.com>
2025-02-01 21:19:18 -0800

    new

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <> 
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[-8%]
-    py- flex items-center text-center z-50 sm:max-xl:p-9'>
+    py- flex items-center text-center z-50 '>
      
        <ul className='md:flex md:flex-col gap-4 px-1 py-20
           fixed-left-64 top-0 bottom-0 w-64 z-50 text-3xl '>
@@ -18,7 +18,7 @@ const Navbar = () => {
           <div className='justify-center fixed-center px-5 lg:px-8 items-center'>
           <Link href="/">
       <Image src="/Images/unnamed.jpg" 
-      className=' max-w-lg max-h-lg w-full'
+      className=' max-w-lg max-h-lg '
      style={{ alignSelf: "center"}}
       width={400}
       height={400}

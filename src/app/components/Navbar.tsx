3aa1c6b6c17520964f7398b0a225870032c53a33
Edvin23 <150404642+Edvin23@users.noreply.github.com>
2025-02-02 15:28:13 -0800

    rework

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,9 +7,9 @@ const Navbar = () => {
   return (
     <> 
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[-8%]
-    py- flex items-center text-center z-50 sm:max-xl '>
+    py- flex  items-center text-center z-50 sm:max-xl   '>
      
-          <ul className='text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl mt-6 h-full md:flex md:flex-col gap-4 px-4 py-2 md:px-6 md:py-4
+          <ul className='text-xl fixed-col sm:text-sm md:text-base lg:text-lg xl:text-xl mt-6 h-full md:flex md:flex-col gap-4 px-4 py-2 md:px-6 md:py-4
            lg:px-8 lg:py-6 
           fixed-left-64 top-0 bottom-0 w-64 z-50 text-left '>
         <li><Link href="/About">about</Link></li>
@@ -21,7 +21,7 @@ const Navbar = () => {
           <div className='justify-center fixed-center px-5 lg:px-8 items-center sm:max-xl:p-9'>
           <Link href="/">
       <Image src="/Images/unnamed.jpg" 
-      className="max-w-xl sm:w-[80%] md:w-4/5 lg:w-3/5 xl:w-1/2"
+      className="max-w-xl sm:w-[80%] md:w-4/5 lg:w-3/5 xl:w-1/2 mx-auto"
      style={{ alignSelf: "center"}}
      width={600}
      height={600}

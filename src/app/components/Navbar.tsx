11160023c5a944a65fd653a37d5b8bec83d47c02
Edvin23 <150404642+Edvin23@users.noreply.github.com>
2025-02-02 03:12:24 -0800

    media screen

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,15 +7,15 @@ const Navbar = () => {
   return (
     <> 
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[-8%]
-    py- flex items-center text-center z-50 '>
+    py- flex items-center text-center z-50 sm:max-xl '>
      
        <ul className='md:flex md:flex-col gap-4 px-1 py-20
-          fixed-left-64 top-0 bottom-0 w-64 z-50 text-3xl '>
+          fixed-left-64 top-0 bottom-0 w-64 z-50 text-3xl sm:max-xl:p-9'>
         <li><Link href="/About">about</Link></li>
         <li><Link href="/Portfolio">portfolio</Link></li>
         <li><Link href="/Contact">contact</Link></li>
           </ul>    
-          <div className='justify-center fixed-center px-5 lg:px-8 items-center'>
+          <div className='justify-center fixed-center px-5 lg:px-8 items-center sm:max-xl:p-9'>
           <Link href="/">
       <Image src="/Images/unnamed.jpg" 
       className=' max-w-lg max-h-lg '
@@ -23,7 +23,7 @@ const Navbar = () => {
       width={400}
       height={400}
       layout='responsive'
-      sizes='100vw,50vw'
+      
        alt='your friend'>
        </Image>
     </Link>

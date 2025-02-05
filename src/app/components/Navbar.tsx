c3c1c92ca42c28fdaf51694b58c38c8379270755
Edvin23 <150404642+Edvin23@users.noreply.github.com>
2025-02-04 19:11:08 -0800

    new navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,24 +6,22 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <> 
-    <nav className='w-full fixed px-5 lg:px-8 xl:px-[-8%]
-    py- flex  items-center text-center z-50 sm:max-xl   '>
+    <nav className='w-full fixed top-0 left-0 px-5 lg:px-8 flex 
+    items-center text-center z-50 py-4  '>
      
-          <ul className='text-xl fixed-col sm:text-sm md:text-base lg:text-lg xl:text-xl mt-6 h-full md:flex md:flex-col gap-4 px-4 py-2 md:px-6 md:py-4
-           lg:px-8 lg:py-6 
-          fixed-left-64 top-0 bottom-0 w-64 z-50 text-left '>
-        <li><Link href="/About">about</Link></li>
-        <li><Link href="/Portfolio">portfolio</Link></li>
-        <li><Link href="/Contact">contact</Link></li>
+          <ul className='text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl  sm:gap-4
+          px-2 py-4 sm:py-4 md:px-6 md:py-10 lg:px-10 lg:py-10 w-64 text-center '>
+        <li className='py-2 sm:py-3'><Link href="/About">about</Link></li>
+        <li className='py-2 sm:py-3'><Link href="/Portfolio">portfolio</Link></li>
+        <li className='py-2 sm:py-3'><Link href="/Contact">contact</Link></li>
           </ul>  
           
 
-          <div className='justify-center fixed-center px-5 lg:px-8 items-center sm:max-xl:p-9'>
+          <div className='flex justify-center items-center w-full px-5 lg:px-8 mt-4'>
           <Link href="/">
       <Image src="/Images/unnamed.jpg" 
-      className="max-w-xl sm:w-[80%] md:w-4/5 lg:w-3/5 xl:w-1/2 mx-auto"
-     style={{ alignSelf: "center"}}
-     width={600}
+      className="w-[95%] sm:w-[85%] md:w-4/5 lg:w-3/5 xl:w-1/2 mx-auto items-center"
+      width={600}
      height={600}
       layout='responsive'
        alt='your friend'>

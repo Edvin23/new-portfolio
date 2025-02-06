7018cf7bed3e7adea03a74986d73b7ba31e35d54
Edvin23 <150404642+Edvin23@users.noreply.github.com>
2025-02-06 15:35:03 -0800

    rework nav and images

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div className='max-w-3xl text-center mx-auto h-screen flex flex-col
-    items-center justify-center gap-20'>
-        <div className=''>
+    <div className='max-w-3xl mx-auto h-screen flex
+    items-center justify-center text-center'>
+        <div className='relative'>
         </div>
-            <div></div>
+          
             <Image src="/Images/page1.jpg" 
-            className='flex  max-w-4xl px-50 ' 
+            className='flex  max-w-4xl  mx-auto object-cover -mt-80' 
             width={1080}
             height={800}
             alt='Home page'></Image>  
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,24 +6,23 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <> 
-    <nav className='w-full fixed top-0 left-0 px-5 lg:px-8 flex 
-    items-center text-center z-50 py-4  '>
+    <nav className='w-full flex flex-row lg:flex-row items-center justify-between px-5 lg:px-8 py-4
+     z-50  '>
      
-          <ul className='text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl  sm:gap-4
-          px-2 py-4 sm:py-4 md:px-6 md:py-10 lg:px-10 lg:py-10 w-64 text-center '>
+          <ul className=' gap-6 test-sm md:text-base lg:text-lg xl:text-xl '>
         <li className='py-2 sm:py-3'><Link href="/About">about</Link></li>
         <li className='py-2 sm:py-3'><Link href="/Portfolio">portfolio</Link></li>
         <li className='py-2 sm:py-3'><Link href="/Contact">contact</Link></li>
           </ul>  
           
 
-          <div className='flex justify-center items-center w-full px-5 lg:px-8 mt-4'>
+          <div className='mt-4 lg:mt-1'>
           <Link href="/">
       <Image src="/Images/unnamed.jpg" 
-      className="w-[95%] sm:w-[85%] md:w-4/5 lg:w-3/5 xl:w-1/2 mx-auto items-center"
-      width={600}
-     height={600}
-      layout='responsive'
+      className="w-full sm:w-3/5 md:w-3/5 lg:w-3/5 max-w-[1200px] mx-auto "
+      width={1200}
+     height={1200}
+      
        alt='your friend'>
        </Image>
     </Link>

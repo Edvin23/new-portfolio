321d4b74b399abd1a5325239431654e5839999b4
Edvin23 <150404642+Edvin23@users.noreply.github.com>
2025-02-11 14:20:02 -0800

    new font

--- src/app/Portfolio/page.tsx
+++ src/app/Portfolio/page.tsx
@@ -7,15 +7,15 @@ const Portfolio = () => {
   return (
     <>
     <Navbar />
-    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col
+    <div className='w-11/12 max-w-3xl text-center mx-auto  flex flex-col
     items-center justify-center gap-4'>
         <div className='text-3xl sm:text-6xl lg:text-[66px]'>
       
-    <Link href='/Portfolio/Anteeok' className='text-3xl sm:text-6xl lg:text-[66px]'>Anteeok</Link>
+    <Link href='/Portfolio/Anteeok' className='text-3xl sm:text-6xl lg:text-[66px]'>anteeok</Link>
     <div>
-    <Link href="/Portfolio/Direction"> Direction</Link>
+    <Link href="/Portfolio/Direction"> direction</Link>
     </div>
-    <Link href="/Portfolio/Design">Design</Link>
+    <Link href="/Portfolio/Design">design</Link>
     </div>
     </div>
     </>
